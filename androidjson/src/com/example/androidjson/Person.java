5d875e7d8bbd5ff326c90d2210b074708cfe075d
saipm <saisreem@mobileintegration-group.com>
2015-11-08 19:22:39 -0800

    Android Project 'R cannot be resolved to a variable'

---
+++ androidjson/src/com/example/androidjson/Person.java
@@ -0,0 +1,29 @@
+package com.example.androidjson;
+
+public class Person {
+ 
+    private String name;
+    private String country;
+    private String twitter;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getTwitter() {
+		return twitter;
+	}
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+ 
+
+ 
+}
